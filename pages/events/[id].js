559155e20d72e1c8db6d57da8fa202032e78e255
Mohamed Hesham <moohisham2000@gmail.com>
2022-08-29 15:47:57 +0200

    feat
    Layout,event detial components

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,9 +1,30 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
 
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/events detail/EventSummary";
+import EventLogistics from "../../components/events detail/EventLogistics";
+import EventContent from "../../components/events detail/EventContent";
 const EventDetailPage = () => {
+  const router = useRouter();
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Event Found!</p>;
+  }
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
